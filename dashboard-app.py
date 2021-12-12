f35e401625d3733b6f597c8a66fe3c8bfabcf202
ingevijsma <91197377+ingevijsma@users.noreply.github.com>
2021-12-12 19:12:33 +0100

    Update dashboard-app.py

--- dashboard-app.py
+++ dashboard-app.py
@@ -26,19 +26,27 @@ st.markdown("""
 Tekst
 """)
 
-Aviation_and_weather_in_July_and_August = pd.read_csv('Aviation and weather in July and August.csv')
-Average_aviation_July_and_August_and_weather_all_year = pd.read_csv('Average aviation July and August and weather all year.csv')
-Aviation_and_weather_all_year = pd.read_csv('Aviation and weather all year.csv')
+# Aviation_and_weather_in_July_and_August = pd.read_csv('Aviation and weather in July and August.csv')
+# Average_aviation_July_and_August_and_weather_all_year = pd.read_csv('Average aviation July and August and weather all year.csv')
+# Aviation_and_weather_all_year = pd.read_csv('Aviation and weather all year.csv')
   
-df = st.radio(label = "Select period:", 
-              options = [Aviation_and_weather_in_July_and_August, 
-                         Average_aviation_July_and_August_and_weather_all_year, 
-                         Aviation_and_weather_all_year])
+# df = st.radio(label = "Select period:", 
+#               options = [Aviation_and_weather_in_July_and_August, 
+#                          Average_aviation_July_and_August_and_weather_all_year, 
+#                          Aviation_and_weather_all_year])
+
+df = pd.read_csv('ALL2.csv')
               
 x = st.radio(label = "Select type of passengers:", 
-             options = ['Total number of passengers', 
-                        'Total passengers arriving', 
-                        'Total passengers departing'])
+             options = ['Total number of passengers (July, August)', 
+                        'Total passengers arriving (July, August)', 
+                        'Total passengers departing (July, August)', 
+                        'Total number of passengers (July, August - All)', 
+                        'Total passengers arriving (July, August - All)', 
+                        'Total passengers departing (July, August - All), 
+                        'Total number of passengers (All)', 
+                        'Total passengers arriving (All)', 
+                        'Total passengers departing (All)'])
 
 y = st.radio(label = "Select weather factor:", 
              options = ['TG', 
