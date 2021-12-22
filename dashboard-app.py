ba283626455c924f840e2f9b6f6ca54ddda22923
ingevijsma <91197377+ingevijsma@users.noreply.github.com>
2021-12-22 14:16:21 +0100

    Update dashboard-app.py

--- dashboard-app.py
+++ dashboard-app.py
@@ -17,9 +17,9 @@ st.warning("*Disclaimer: The years 2020 and 2021 are excluded due to the COVID-1
 #Insert information
 st.info("""
 Three different studies:\n
-1. **Summer holiday period (July and August)**: Investigate whether the weather during the summer influences the behaviour of (arriving/departing) (holiday) passengers during the summer period.\n
-2. **Summer holiday period (July and August) and the weather during the year**: Investigate whether the weather during the year influences the behaviour of (arriving/departing) (early booking) (holiday) passengers in the summer period.\n 
-3. **Year-round weather**: Investigate whether the weather during the year influences the behaviour of (arriving/departing) passengers all year round.\n
+1. **Summer holiday period**: Investigate whether the weather during the summer influences the behaviour of (arriving/departing) (holiday) passengers during the summer period.\n
+2. **Aviation summer holiday period and weather during the year**: Investigate whether the weather during the year influences the behaviour of (arriving/departing) (early booking) (holiday) passengers in the summer period.\n 
+3. **Year-round aviation and weather**: Investigate whether the weather during the year influences the behaviour of (arriving/departing) passengers all year round.\n
 """)
 
 #Upload dataframe
