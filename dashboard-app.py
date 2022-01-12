20529e16c2437bd4d257368b0d07d0a6d666093e
ingevijsma <91197377+ingevijsma@users.noreply.github.com>
2022-01-12 18:46:05 +0100

    Update dashboard-app.py

--- dashboard-app.py
+++ dashboard-app.py
@@ -18,10 +18,10 @@ st.title('Aviation and Weather in the Netherlands')
 st.subheader('From 2000 to 2019')
 
 #Insert warning
-col1.st.warning("*Disclaimer: The years 2020 and 2021 are excluded due to the COVID-19 pandemic!*")
+col1.warning("*Disclaimer: The years 2020 and 2021 are excluded due to the COVID-19 pandemic!*")
 
 #Insert information
-col1.st.info("""
+col1.info("""
 Three different studies:\n
 1. **Summer holiday period**: Investigate whether the weather during the summer influences the behaviour of (arriving/departing) (holiday) passengers during the summer period.\n
 2. **Aviation during the summer holiday period and weather during the year**: Investigate whether the weather during the year influences the behaviour of (arriving/departing) (early booking) (holiday) passengers in the summer period.\n 
@@ -32,7 +32,7 @@ Three different studies:\n
 df = pd.read_csv('STREAMLIT.csv')
 
 #Make selectbox of the different studies
-period = col2.st.selectbox(label = 'Select study:', 
+period = col2.selectbox(label = 'Select study:', 
 		      options = ['Study 1', 'Study 2', 'Study 3'], 
 		      help = 'Select the desired study here. Study 1 examines only the months of July and August. Study 2 examines only the months of July and August for aviation and the entire year for weather factors. Study 3 examines the entire year.')
 
@@ -75,35 +75,35 @@ if period == 'Study 3':
        
 #Make radio and multiselect per study for different types of passengers and weather factors, respectively
 if period == 'Study 1':	
-	x = col2.st.radio(label = "Select type of passengers:", 
+	x = col2.radio(label = "Select type of passengers:", 
 		     options = ['Total number of passengers (1)', 
 				'Total passengers arriving (1)', 
 				'Total passengers departing (1)'], 
 		     help = 'Select the desired type of passenger here. Option 1 is both arriving and departing. Option 2 is arriving only and option 3 is departing only.')
 
-	y = col2.st.multiselect(label = "Select weather factor(s):", 
+	y = col2.multiselect(label = "Select weather factor(s):", 
 			   options = ['SQ (1)', 'TX (1)', 'RHX (1)', 'TG (1)', 'RH (1)'], 
 			   help = 'Select the desired weather factor(s) here. Previous research has determined the five most important weather factors related to passenger volume. These weather factors are listed in order from left to right in the multiselect and from top to bottom in the legend to the figure in terms of importance.')
 
 if period == 'Study 2':	
-	x = col2.st.radio(label = "Select type of passengers:", 
+	x = col2.radio(label = "Select type of passengers:", 
 		     options = ['Total number of passengers (2)', 
 				'Total passengers arriving (2)', 
 				'Total passengers departing (2)'], 
 		     help = 'Select the desired type of passenger here. Option 1 is both arriving and departing. Option 2 is arriving only and option 3 is departing only.')
 
-	y = col2.st.multiselect(label = "Select weather factor(s):", 
+	y = col2.multiselect(label = "Select weather factor(s):", 
 			   options = ['SQ (2)', 'TX (2)', 'TG (2)', 'RH (2)', 'RHX (2)'], 
 			   help = 'Select the desired weather factor(s) here. Previous research has determined the five most important weather factors related to passenger volume. These weather factors are listed in order from left to right in the multiselect and from top to bottom in the legend to the figure in terms of importance.')
 
 if period == 'Study 3':	
-	x = col2.st.radio(label = "Select type of passengers:", 
+	x = col2.radio(label = "Select type of passengers:", 
 		     options = ['Total number of passengers (3)', 
 				'Total passengers arriving (3)', 
 				'Total passengers departing (3)'], 
 		     help = 'Select the desired type of passenger here. Option 1 is both arriving and departing. Option 2 is arriving only and option 3 is departing only.')
 
-	y = col2.st.multiselect(label = "Select weather factor(s):", 
+	y = col2.multiselect(label = "Select weather factor(s):", 
 			   options = ['TX (3)', 'TG (3)', 'TN (3)', 'SQ (3)', 'DR (3)'], 
 			   help = 'Select the desired weather factor(s) here. Previous research has determined the five most important weather factors related to passenger volume. These weather factors are listed in order from left to right in the multiselect and from top to bottom in the legend to the figure in terms of importance.')
 
