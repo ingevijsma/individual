0fbb848caf25a11ee8aa244cd21f564cca548370
ingevijsma <91197377+ingevijsma@users.noreply.github.com>
2022-01-12 18:52:34 +0100

    Update dashboard-app.py

--- dashboard-app.py
+++ dashboard-app.py
@@ -14,27 +14,27 @@ st.set_page_config(layout = "wide")
 st.title('Aviation and Weather in the Netherlands')
 st.subheader('From 2000 to 2019')
 
-#Set up columns
-col1, col2 = st.columns(2)
-
 #Insert warning
-col1.warning("*Disclaimer: The years 2020 and 2021 are excluded due to the COVID-19 pandemic!*")
+st.warning("*Disclaimer: The years 2020 and 2021 are excluded due to the COVID-19 pandemic!*")
 
 #Insert information
-col1.info("""
+st.info("""
 Three different studies:\n
 1. **Summer holiday period**: Investigate whether the weather during the summer influences the behaviour of (arriving/departing) (holiday) passengers during the summer period.\n
 2. **Aviation during the summer holiday period and weather during the year**: Investigate whether the weather during the year influences the behaviour of (arriving/departing) (early booking) (holiday) passengers in the summer period.\n 
 3. **Year-round aviation and weather**: Investigate whether the weather during the year influences the behaviour of (arriving/departing) passengers all year round.\n
 """)
 
+#Set up columns
+col1, col2 = st.columns(2)
+
 #Upload dataframe
 df = pd.read_csv('STREAMLIT.csv')
 
 #Make selectbox of the different studies
-period = col2.selectbox(label = 'Select study:', 
-		      options = ['Study 1', 'Study 2', 'Study 3'], 
-		      help = 'Select the desired study here. Study 1 examines only the months of July and August. Study 2 examines only the months of July and August for aviation and the entire year for weather factors. Study 3 examines the entire year.')
+period = col1.selectbox(label = 'Select study:', 
+			options = ['Study 1', 'Study 2', 'Study 3'], 
+			help = 'Select the desired study here. Study 1 examines only the months of July and August. Study 2 examines only the months of July and August for aviation and the entire year for weather factors. Study 3 examines the entire year.')
 
 #Make three lists for different studies
 if period == 'Study 1':
@@ -75,47 +75,48 @@ if period == 'Study 3':
        
 #Make radio and multiselect per study for different types of passengers and weather factors, respectively
 if period == 'Study 1':	
-	x = col2.radio(label = "Select type of passengers:", 
-		     options = ['Total number of passengers (1)', 
-				'Total passengers arriving (1)', 
-				'Total passengers departing (1)'], 
-		     help = 'Select the desired type of passenger here. Option 1 is both arriving and departing. Option 2 is arriving only and option 3 is departing only.')
+	x = col1.radio(label = "Select type of passengers:", 
+		       options = ['Total number of passengers (1)', 
+				  'Total passengers arriving (1)', 
+				  'Total passengers departing (1)'], 
+		       help = 'Select the desired type of passenger here. Option 1 is both arriving and departing. Option 2 is arriving only and option 3 is departing only.')
 
-	y = col2.multiselect(label = "Select weather factor(s):", 
-			   options = ['SQ (1)', 'TX (1)', 'RHX (1)', 'TG (1)', 'RH (1)'], 
-			   help = 'Select the desired weather factor(s) here. Previous research has determined the five most important weather factors related to passenger volume. These weather factors are listed in order from left to right in the multiselect and from top to bottom in the legend to the figure in terms of importance.')
+	y = col1.multiselect(label = "Select weather factor(s):", 
+			     options = ['SQ (1)', 'TX (1)', 'RHX (1)', 'TG (1)', 'RH (1)'], 
+			     help = 'Select the desired weather factor(s) here. Previous research has determined the five most important weather factors related to passenger volume. These weather factors are listed in order from left to right in the multiselect and from top to bottom in the legend to the figure in terms of importance.')
 
 if period == 'Study 2':	
-	x = col2.radio(label = "Select type of passengers:", 
-		     options = ['Total number of passengers (2)', 
-				'Total passengers arriving (2)', 
-				'Total passengers departing (2)'], 
-		     help = 'Select the desired type of passenger here. Option 1 is both arriving and departing. Option 2 is arriving only and option 3 is departing only.')
+	x = col1.radio(label = "Select type of passengers:", 
+		       options = ['Total number of passengers (2)', 
+				  'Total passengers arriving (2)', 
+				  'Total passengers departing (2)'], 
+		       help = 'Select the desired type of passenger here. Option 1 is both arriving and departing. Option 2 is arriving only and option 3 is departing only.')
 
-	y = col2.multiselect(label = "Select weather factor(s):", 
-			   options = ['SQ (2)', 'TX (2)', 'TG (2)', 'RH (2)', 'RHX (2)'], 
-			   help = 'Select the desired weather factor(s) here. Previous research has determined the five most important weather factors related to passenger volume. These weather factors are listed in order from left to right in the multiselect and from top to bottom in the legend to the figure in terms of importance.')
+	y = col1.multiselect(label = "Select weather factor(s):", 
+			     options = ['SQ (2)', 'TX (2)', 'TG (2)', 'RH (2)', 'RHX (2)'], 
+			     help = 'Select the desired weather factor(s) here. Previous research has determined the five most important weather factors related to passenger volume. These weather factors are listed in order from left to right in the multiselect and from top to bottom in the legend to the figure in terms of importance.')
 
 if period == 'Study 3':	
-	x = col2.radio(label = "Select type of passengers:", 
-		     options = ['Total number of passengers (3)', 
-				'Total passengers arriving (3)', 
-				'Total passengers departing (3)'], 
-		     help = 'Select the desired type of passenger here. Option 1 is both arriving and departing. Option 2 is arriving only and option 3 is departing only.')
+	x = col1.radio(label = "Select type of passengers:", 
+		       options = ['Total number of passengers (3)', 
+				  'Total passengers arriving (3)', 
+				  'Total passengers departing (3)'], 
+		       help = 'Select the desired type of passenger here. Option 1 is both arriving and departing. Option 2 is arriving only and option 3 is departing only.')
 
-	y = col2.multiselect(label = "Select weather factor(s):", 
-			   options = ['TX (3)', 'TG (3)', 'TN (3)', 'SQ (3)', 'DR (3)'], 
-			   help = 'Select the desired weather factor(s) here. Previous research has determined the five most important weather factors related to passenger volume. These weather factors are listed in order from left to right in the multiselect and from top to bottom in the legend to the figure in terms of importance.')
+	y = col1.multiselect(label = "Select weather factor(s):", 
+			     options = ['TX (3)', 'TG (3)', 'TN (3)', 'SQ (3)', 'DR (3)'],
+			     help = 'Select the desired weather factor(s) here. Previous research has determined the five most important weather factors related to passenger volume. These weather factors are listed in order from left to right in the multiselect and from top to bottom in the legend to the figure in terms of importance.')
 
-#Add black line
-st.markdown('***')
-st.markdown("<h3 style='text-align: center; color: black;'>Number of passengers (arriving/departing) versus weather factors</h3>", unsafe_allow_html = True)
-st.markdown('***')
+# #Add black line
+# st.markdown('***')
+# st.markdown("<h3 style='text-align: center; color: black;'>Number of passengers (arriving/departing) versus weather factors</h3>", unsafe_allow_html = True)
+# st.markdown('***')
 
 #Code scatterplot with and without trendline
-fig_scatterplot_trendline = st.checkbox('Trendline', value = False)
+fig_scatterplot_trendline = col2.checkbox('Trendline', value = False)
 if fig_scatterplot_trendline == True:
   fig1 = px.scatter(data_frame = df, 
+		    title = 'Number of passengers (arriving/departing) versus weather factors',
                     x = x, 
                     y = y, 
                     trendline = 'ols', 
@@ -128,12 +129,12 @@ if fig_scatterplot_trendline == False:
                     y = y, 
 		    log_y = True)
 
-st.plotly_chart(fig1)
+col2.plotly_chart(fig1)
 
 #Insert expander
-with st.expander('More information:'):
-	st.subheader('Abbreviation of weather factors with units:')
-	st.markdown("""TG = Daily mean temperature (in degrees Celsius)\n
+with col2.expander('More information:'):
+	col2.subheader('Abbreviation of weather factors with units:')
+	col2.markdown("""TG = Daily mean temperature (in degrees Celsius)\n
 TN = Minimum temperature (in degrees Celsius)\n
 TX = Maximum temperature (in degrees Celsius)\n
 SQ = Sunshine duration (in hours)\n
